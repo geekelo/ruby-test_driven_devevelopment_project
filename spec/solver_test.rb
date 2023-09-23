a95acaf212af9a40465b4fada6fc5119bf362f6e
geekelo <efuelight12@gmail.com>
2023-09-23 21:39:32 +0100

    Write test for positive integers

--- spec/solver_test.rb
+++ spec/solver_test.rb
@@ -4,4 +4,9 @@ class TestSolver < Minitest::Test
   def setup
     @solver = Solver.new
   end
+
+  def test_factorial_positive_num
+    assert_equal 6, @solver.factorial(3)
+    assert_equal 1, @solver.factorial(0)
+  end
 end
